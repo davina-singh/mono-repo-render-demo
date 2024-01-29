3e6061dcd0b77f71457430aff0362dc79129125e
DAVINA SINGH <davina_singh@outlook.com>
2024-01-29 15:06:09 +0000

    initial

---
+++ client/main.js
@@ -0,0 +1,17 @@
+
+const container = document.getElementById("app")
+
+async function fetchImages() {
+  const data = await fetch(`${import.meta.env.VITE_server}/images`)
+  const result = await data.json()
+  console.log(result)
+  return result 
+}
+
+const images = await fetchImages()
+console.log(images)
+images.forEach(element => {
+  let image = document.createElement("img")
+  image.src = element.image
+  document.body.appendChild(image)
+});
